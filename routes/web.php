e680746b985e8e914c179e35862e29cfc2000339
Luis <luisrevilla20@gmail.com>
2021-05-17 17:32:35 -0500

    URL shortener ready

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShortLinkController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Route::get('generate-shorten-link', 'ShortLinkController@index');
+Route::get('generate-shorten-link', 'App\Http\Controllers\ShortLinkController@index');
+
+//Route::post('generate-shorten-link', 'ShortLinkController@store')->name('generate.shorten.link.post');
+Route::post('generate-shorten-link', 'App\Http\Controllers\ShortLinkController@store')->name('generate.shorten.link.post');
+
+//Route::get('{code}', 'ShortLinkController@shortenLink')->name('shorten.link');
+Route::get('{code}', 'App\Http\Controllers\ShortLinkController@shortenLink')->name('shorten.link');
 
 Route::get('/', function () {
     return view('welcome');
